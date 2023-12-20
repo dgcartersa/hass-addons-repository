d5c0642f46b193ba53cf213210c7a01b74e8ce8c
Darren Carter <dc@pfortner.co.za>
2023-12-20 23:09:44 +0200

    Updates

--- nightscout/build_local_aarch64.sh
+++
@@ -1,1 +0,0 @@
-docker build --build-arg BUILD_FROM="ghcr.io/hassio-addons/ubuntu-base/aarch64:6.1.3" --build-arg BUILD_ARCH=aarch64 --progress plain --no-cache -t dgcartersa/image-aarch64-addon-nightscout:$1 .
--- nightscout/build_local_amd64.sh
+++
@@ -1,1 +0,0 @@
-docker build --build-arg BUILD_FROM="ghcr.io/hassio-addons/ubuntu-base:8.1.1" --build-arg BUILD_ARCH=amd64 --progress plain --no-cache -t dgcartersa/image-amd64-addon-nightscout:$1 .
