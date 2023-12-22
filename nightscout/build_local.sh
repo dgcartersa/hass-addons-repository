881f79148848b5b88cc2cf696506daf12d864a6f
Darren Carter <dc@pfortner.co.za>
2023-12-22 22:30:18 +0200

    V15 Reabse

--- nightscout/build_local.sh
+++ nightscout/build_local.sh
@@ -1,1 +1,1 @@
-docker build --build-arg BUILD_FROM="ghcr.io/hassio-addons/ubuntu-base:8.1.1" --build-arg BUILD_ARCH=$1 --progress plain --no-cache -t dgcartersa/image-$1-addon-nightscout:$2 .
+docker build --build-arg BUILD_FROM="ghcr.io/hassio-addons/ubuntu-base:9.0.2" --build-arg BUILD_ARCH=$1 --progress plain --no-cache -t dgcartersa/image-$1-addon-nightscout:$2 .
